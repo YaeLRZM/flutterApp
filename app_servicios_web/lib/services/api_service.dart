198625c7b8049eb232a9f9284b5d9cf939ecabed
JoelMatadamas <cristhianmatadamas@gmail.com>
2026-07-17 22:14:43 -0600

    mejora para la mac

--- app_servicios_web/lib/services/api_service.dart
+++ app_servicios_web/lib/services/api_service.dart
@@ -1,11 +1,28 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  // Cambia esta URL por la IP de tu servidor local o dominio
-  // 10.0.2.2 es para el emulador de Android apuntando al localhost de la PC.
-  static const String baseUrl = 'http://10.0.2.2:8000/api';
+  // Cambia esto dependiendo de si usas emulador o teléfono físico
+  static const bool useAndroidEmulator = false; // true si usas el emulador de Android Studio
+
+static String get baseUrl {
+  if (kIsWeb) {
+    return 'http://127.0.0.1:8000/api';
+  }
+
+  if (Platform.isAndroid) {
+    if (useAndroidEmulator) {
+      return 'http://10.0.2.2:8000/api'; // SIEMPRE así para el emulador
+    } else {
+      return 'http://127.0.0.1:8001/api'; // requiere adb reverse (ver abajo)
+    }
+  }
+
+  return 'http://127.0.0.1:8000/api';
+}
 
   Future<Map<String, dynamic>> login(String email, String password) async {
     try {
@@ -15,14 +32,15 @@ class ApiService {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
         },
-        body: jsonEncode({'email': email, 'password': password}),
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+        }),
       );
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
 
-        // Asumiendo que tu API devuelve un token en un campo 'access_token' o 'token'
-        // Esto depende de cómo lo configuraste en tu AuthController de Laravel.
         if (data.containsKey('access_token')) {
           final prefs = await SharedPreferences.getInstance();
           await prefs.setString('jwt_token', data['access_token']);
@@ -33,11 +51,15 @@ class ApiService {
         return {
           'success': false,
           'message':
-              jsonDecode(response.body)['message'] ?? 'Error de credenciales',
+              jsonDecode(response.body)['message'] ??
+              'Error de credenciales',
         };
       }
     } catch (e) {
-      return {'success': false, 'message': 'Error de conexión: $e'};
+      return {
+        'success': false,
+        'message': 'Error de conexión: $e',
+      };
     }
   }
 }
